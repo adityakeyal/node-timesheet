7c9018e557a0c26228475daaf0b6b02820c5d472
Aditya Keyal <adityak@nrifintech.com>
2018-03-01 08:16:26 +0530

    refactored for better test

--- test/date-test.js
+++ test/date-test.js
@@ -31,5 +31,54 @@ describe('Formatted date module', function(){
 
     });
 
+    it('returns range of days in mid of january' , function(){
+
+        var date = new Date();
+        date.setDate(14);
+        date.setMonth(0);
+        date.setFullYear(2018);
+        var range = datemod.fetchDaysInMonth(date);
+        expect(range.length).to.eql(14);
+
+    });
+
+    it('returns range of days in 1st day of january' , function(){
+
+        var date = new Date();
+        date.setDate(1);
+        date.setMonth(0);
+        date.setFullYear(2018);
+        var range = datemod.fetchDaysInMonth(date);
+        expect(range.length).to.eql(1);
+
+    });
+
+    
+    it('returns range of days in last day of january' , function(){
+
+        var date = new Date();
+        date.setDate(31);
+        date.setMonth(0);
+        date.setFullYear(2018);
+        var range = datemod.fetchDaysInMonth(date);
+
+        expect(range.length).to.eql(31);
+
+    });
+
+    it('returns business days in month of january' , function(){
+
+
+        var date = new Date();
+        date.setDate(31);
+        date.setMonth(0);
+        date.setFullYear(2018);
+        var range = datemod.fetchBusinessDaysInMonth(date);
+
+        expect(range.length).to.eql(23);
+        
+
+    });
+
 
 });
